52ef2ee7bc11ce11023bae7dc09e9913bd20da72
Dmitriy Gerasimenko <dvgerasimenko@avito.ru>
2024-02-01 08:15:52 +0300

    Add linters and formatters

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,27 @@
+return {
+        "nvimtools/none-ls.nvim",
+        config = function()
+                local none_ls = require("null-ls")
+
+                none_ls.setup({
+                        sources = {
+                                none_ls.builtins.formatting.stylua,
+                                none_ls.builtins.diagnostics.eslint,
+                                none_ls.builtins.formatting.eslint,
+                                none_ls.builtins.diagnostics.golangci_lint,
+                                none_ls.builtins.formatting.gofumpt,
+                                none_ls.builtins.diagnostics.stylelint,
+                                none_ls.builtins.formatting.stylelint,
+                                none_ls.builtins.formatting.phpcsfixer,
+                                none_ls.builtins.diagnostics.phpstan,
+                                none_ls.builtins.formatting.fixjson,
+                                none_ls.builtins.diagnostics.jsonlint,
+                                none_ls.builtins.diagnostics.checkmake,
+                                none_ls.builtins.formatting.shfmt,
+                                none_ls.builtins.diagnostics.shellcheck,
+                        },
+                })
+
+                vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, {})
+        end,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,29 @@
 return {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-                local treesitter_configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local treesitter_configs = require("nvim-treesitter.configs")
 
-                treesitter_configs.setup({
-                        ensure_installed = {
-                                "lua",
-                                "javascript",
-                                "typescript",
-                                "html",
-                                "json",
-                                "go",
-                                "css",
-                                "sql",
-                                "yaml",
-                                "xml",
-                                "php",
-                                "markdown",
-                                "python",
-                                "make"
-                        },
-                        highlight = { enable = true },
-                        indent = { enable = true },
-                })
-        end
+		treesitter_configs.setup({
+			auto_install = { enabled = true },
+			ensure_installed = {
+				"lua",
+				"javascript",
+				"typescript",
+				"html",
+				"json",
+				"go",
+				"css",
+				"sql",
+				"yaml",
+				"xml",
+				"php",
+				"markdown",
+				"python",
+				"make",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
-
