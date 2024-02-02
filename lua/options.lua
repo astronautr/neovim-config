f49d55c292d243873090ea2c3f106410f0866f80
Dmitriy Gerasimenko <dvgerasimenko@avito.ru>
2024-02-02 12:56:36 +0300

    Add auto indent on newline

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,10 @@ vim.cmd("set smartindent")
 vim.cmd("set clipboard+=unnamed")
 vim.cmd("set nu")
 vim.cmd("set rnu")
+
+vim.o.mouse = 'a'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,46 +1,50 @@
 return {
-        {
-                "lukas-reineke/indent-blankline.nvim",
-                config = function(_, opts)
-                        require("ibl").setup(opts)
-                end,
-                opts = {
-                        indent = {
-                                char = "│",
-                                tab_char = "│",
-                        },
-                        scope = { enabled = false },
-                        exclude = {
-                                filetypes = {
-                                        "help",
-                                        "alpha",
-                                        "dashboard", "neo-tree",
-                                        "Trouble",
-                                        "trouble",
-                                        "lazy",
-                                        "mason",
-                                        "notify",
-                                        "toggleterm",
-                                        "lazyterm",
-                                },
-                        },
-                },
-                main = "ibl",
-        },
-        {
-                "echasnovski/mini.indentscope",
-                version = false,
-                config = function (_, opts)
-                        local indent = require("mini.indentscope")
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function(_, opts)
+			require("ibl").setup(opts)
+		end,
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+			},
+		},
+		main = "ibl",
+	},
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		config = function(_, opts)
+			local indent = require("mini.indentscope")
 
-                        indent.gen_animation.none()
-                        indent.setup(opts)
-                end,
-                opts = {
-                        symbol = "│",
-                        options = {
-                                try_as_border = true,
-                        },
-                },
-        },
+			indent.gen_animation.none()
+			indent.setup(opts)
+		end,
+		opts = {
+			symbol = "│",
+			options = {
+				try_as_border = true,
+			},
+		},
+	},
+	{
+		"tpope/vim-sleuth",
+	},
 }
