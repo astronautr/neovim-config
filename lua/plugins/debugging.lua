7042936cb32d588f514686863f7fdc845fafdadf
Dmitriy Gerasimenko <dvgerasimenko@avito.ru>
2024-02-06 00:06:12 +0300

    Add chrome debugger

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -11,6 +11,51 @@ return {
 
 		local dap, dapui = require("dap"), require("dapui")
 
+		dap.adapters.chrome = {
+			type = "executable",
+			command = "node",
+			args = { os.getenv("HOME") .. "/Personal_Projects/vscode-chrome-debug/out/src/chromeDebug.js" },
+		}
+
+		dap.configurations.javascriptreact = { -- change this to javascript if needed
+			{
+				type = "chrome",
+				request = "attach",
+				program = "${file}",
+				cwd = vim.fn.getcwd(),
+				sourceMaps = true,
+				protocol = "inspector",
+				port = 9222,
+				webRoot = "${workspaceFolder}",
+			},
+		}
+
+		dap.configurations.javascript = { -- change this to javascript if needed
+			{
+				type = "chrome",
+				request = "attach",
+				program = "${file}",
+				cwd = vim.fn.getcwd(),
+				sourceMaps = true,
+				protocol = "inspector",
+				port = 9222,
+				webRoot = "${workspaceFolder}",
+			},
+		}
+
+		dap.configurations.typescript = { -- change to typescript if needed
+			{
+				type = "chrome",
+				request = "attach",
+				program = "${file}",
+				cwd = vim.fn.getcwd(),
+				sourceMaps = true,
+				protocol = "inspector",
+				port = 9222,
+				webRoot = "${workspaceFolder}",
+			},
+		}
+
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
 		end
@@ -28,5 +73,8 @@ return {
 		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
 		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
 		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+		vim.keymap.set("n", "<Leader>di", ":DapStepInto<CR>")
+		vim.keymap.set("n", "<Leader>dO", ":DapStepOutside<CR>")
+		vim.keymap.set("n", "<Leader>dr", dap.run_to_cursor, {})
 	end,
 }
