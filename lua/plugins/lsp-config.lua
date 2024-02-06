4122c4657bd9fb45445b282fc9d09a7bba3151f6
Dmitriy Gerasimenko <dvgerasimenko@avito.ru>
2024-02-06 21:34:48 +0300

    Add yaml lsp, linter and formatter

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,60 +1,61 @@
 return {
-        {
-                "williamboman/mason.nvim",
-                config = function()
-                        require("mason").setup()
-                end,
-        },
-        {
-                "williamboman/mason-lspconfig.nvim",
-                config = function()
-                        require("mason-lspconfig").setup({
-                                ensure_installed = {
-                                        "lua_ls",
-                                        "gopls",
-                                        "tsserver",
-                                        "cssls",
-                                        "html",
-                                        "marksman",
-                                        "intelephense",
-                                        "jsonls",
-                                        "eslint",
-                                        "dockerls",
-                                        "taplo"
-                                },
-                        })
-                end,
-        },
-        {
-                "neovim/nvim-lspconfig",
-                config = function()
-                        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-                        local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"tsserver",
+					"cssls",
+					"html",
+					"marksman",
+					"intelephense",
+					"jsonls",
+					"eslint",
+					"dockerls",
+					"taplo",
+					"yamlls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-                        lspconfig.lua_ls.setup({ capabilities = capabilities })
-                        lspconfig.gopls.setup({ capabilities = capabilities })
-                        lspconfig.tsserver.setup({ capabilities = capabilities })
-                        lspconfig.cssls.setup({ capabilities = capabilities })
-                        lspconfig.html.setup({ capabilities = capabilities })
-                        lspconfig.marksman.setup({ capabilities = capabilities })
-                        lspconfig.intelephense.setup({ capabilities = capabilities })
-                        lspconfig.jsonls.setup({ capabilities = capabilities })
-                        lspconfig.eslint.setup({ capabilities = capabilities })
-                        lspconfig.dockerls.setup({ capabilities = capabilities })
-                        lspconfig.taplo.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.marksman.setup({ capabilities = capabilities })
+			lspconfig.intelephense.setup({ capabilities = capabilities })
+			lspconfig.jsonls.setup({ capabilities = capabilities })
+			lspconfig.eslint.setup({ capabilities = capabilities })
+			lspconfig.dockerls.setup({ capabilities = capabilities })
+			lspconfig.taplo.setup({ capabilities = capabilities })
+			lspconfig.yamlls.setup({ capabilities = capabilities })
 
-
-                        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-                        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-                        vim.keymap.set({"n", "v"}, "gt", vim.lsp.buf.type_definition, {})
-                        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-                        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-                        vim.keymap.set("n", "gI", vim.lsp.buf.implementation, {})
-                        vim.keymap.set("n", "gi", vim.lsp.buf.incoming_calls, {})
-                        vim.keymap.set("n", "go", vim.lsp.buf.outgoing_calls, {})
-                        vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-                        vim.keymap.set("n", "gp", vim.lsp.buf.signature_help, {})
-                        vim.keymap.set({"n", "v"}, "<leader>cr", vim.lsp.buf.rename,{})
-                end,
-        },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n", "v" }, "gt", vim.lsp.buf.type_definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gI", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.incoming_calls, {})
+			vim.keymap.set("n", "go", vim.lsp.buf.outgoing_calls, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "gp", vim.lsp.buf.signature_help, {})
+			vim.keymap.set({ "n", "v" }, "<leader>cr", vim.lsp.buf.rename, {})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,28 +1,30 @@
 return {
-        "nvimtools/none-ls.nvim",
-        config = function()
-                local none_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local none_ls = require("null-ls")
 
-                none_ls.setup({
-                        sources = {
-                                none_ls.builtins.formatting.stylua,
-                                none_ls.builtins.diagnostics.eslint,
-                                none_ls.builtins.formatting.eslint,
-                                none_ls.builtins.diagnostics.golangci_lint,
-                                none_ls.builtins.formatting.goimports,
-                                none_ls.builtins.diagnostics.stylelint,
-                                none_ls.builtins.formatting.stylelint,
-                                none_ls.builtins.formatting.phpcsfixer,
-                                none_ls.builtins.diagnostics.phpstan,
-                                none_ls.builtins.formatting.fixjson,
-                                none_ls.builtins.diagnostics.jsonlint,
-                                none_ls.builtins.diagnostics.checkmake,
-                                none_ls.builtins.formatting.shfmt,
-                                none_ls.builtins.diagnostics.shellcheck,
-                                none_ls.builtins.diagnostics.hadolint
-                        },
-                })
+		none_ls.setup({
+			sources = {
+				none_ls.builtins.formatting.stylua,
+				none_ls.builtins.diagnostics.eslint,
+				none_ls.builtins.formatting.eslint,
+				none_ls.builtins.diagnostics.golangci_lint,
+				none_ls.builtins.formatting.goimports,
+				none_ls.builtins.diagnostics.stylelint,
+				none_ls.builtins.formatting.stylelint,
+				none_ls.builtins.formatting.phpcsfixer,
+				none_ls.builtins.diagnostics.phpstan,
+				none_ls.builtins.formatting.fixjson,
+				none_ls.builtins.diagnostics.jsonlint,
+				none_ls.builtins.diagnostics.checkmake,
+				none_ls.builtins.formatting.shfmt,
+				none_ls.builtins.diagnostics.shellcheck,
+				none_ls.builtins.diagnostics.hadolint,
+				none_ls.builtins.diagnostics.yamllint,
+				none_ls.builtins.formatting.yamlfix,
+			},
+		})
 
-                vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, {})
-        end,
+		vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
