08c34cdbc0a7bc19a8aa4183dba3414172830356
Dmitriy Gerasimenko <dvgerasimenko@avito.ru>
2024-02-08 17:30:02 +0300

    Add diagnostics, treesitter and grep string shortcuts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,8 +26,11 @@ return {
 
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fl", builtin.grep_string, {})
+			vim.keymap.set({"n", "v"}, "<leader>ft", builtin.treesitter, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set({ "n", "v" }, "<leader>fr", builtin.oldfiles, {})
+			vim.keymap.set({ "n", "v" }, "<leader>fd", builtin.diagnostics, {})
 		end,
 	},
 	{
