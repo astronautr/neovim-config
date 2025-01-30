df9b6b05fe6a0a3fcb7ac82b6b256aa28a4fff99
Dmitriy Gerasimenko <dvgerasimenko@avito.ru>
2025-01-30 14:55:24 +0300

    Revamp config

---
+++ init.lua
@@ -0,0 +1,438 @@
+require('options')
+
+-- [[Менеджер пакетов]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- [[Настройка плагинов]]
+local plugins = {
+    -- Тема
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        pin = true,
+        config = function()
+            require("catppuccin").setup()
+            vim.cmd.colorscheme "catppuccin"
+        end
+    },
+    -- Навигация между панелями tmux и vim
+    {
+        "christoomey/vim-tmux-navigator",
+        pin = true,
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+    -- Поиск по проекту
+    {
+        'nvim-telescope/telescope.nvim',
+        pin = true,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-treesitter/nvim-treesitter',
+
+
+        },
+        config = function()
+            local telescope = require('telescope')
+            local builtin = require('telescope.builtin')
+            local actions = require('telescope.actions')
+
+            telescope.setup({
+                defaults = {
+                    -- Убираем игнорирование .git директории
+                    file_ignore_patterns = {
+                        "node_modules",
+                        "vendor",
+                        ".idea",
+                        "^.git/",
+                        ".digest"
+                    },
+                    mappings = {
+                        i = {
+                            ["<esc>"] = actions.close,
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+                        },
+                    },
+                    preview = {
+                        treesitter = true
+                    }
+                },
+                pickers = {
+                    -- Вывод в поиске скрытых файлов
+                    find_files = {
+                        hidden = true
+                    },
+                    git_files = {
+                        show_untracked = true,
+                    },
+                    live_grep = {
+                        additional_args = function()
+                            return { "--hidden" }
+                        end
+                    },
+                    buffers = {
+                        sort_mru = true,
+                        ignore_current_buffer = false,
+                    },
+                    current_buffer_fuzzy_find = {
+                        skip_empty_lines = true,
+                    },
+                }
+            })
+
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+            vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find git files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find text' })
+            vim.keymap.set('n', '<C-e>', builtin.buffers, { desc = 'Find buffers' })
+            vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, { desc = 'Search in current buffer' })
+        end,
+    },
+    -- Подсветка синтаксиса, навигация, отступы
+    {
+        "nvim-treesitter/nvim-treesitter",
+        pin = true,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects"
+        },
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "lua",
+                    "javascript",
+                    "typescript",
+                    "html",
+                    "css",
+                    "go",
+                    "php",
+                    "json",
+                    "dockerfile",
+                    "yaml",
+                    "tsx",
+                    "sql",
+                    "markdown",
+                    "jsdoc",
+                    "gomod",
+                    "gosum",
+                    "fish",
+                    "make",
+                    "bash"
+                },
+                highlight = { enable = true },
+                indent = { enable = true },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                        },
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        pin = true,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            -- Mason Setup
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",       -- Lua
+                    "ts_ls",        -- JavaScript/TypeScript
+                    "html",         -- HTML
+                    "cssls",        -- CSS
+                    "gopls",        -- Go
+                    "intelephense", -- PHP
+                    "jsonls",       -- JSON
+                    "yamlls",       -- YAML
+                    "dockerls",     -- Docker
+                    "marksman",     -- Markdown
+                    "sqlls",        -- SQL
+                    "eslint",       -- Форматирование eslint
+                },
+                automatic_installation = true,
+            })
+
+            -- Capabilities для автодополнения
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            -- LSP Setup
+            local lspconfig = require("lspconfig")
+
+            -- Базовая функция настройки с capabilities
+            local function setup_lsp(server)
+                lspconfig[server].setup({
+                    capabilities = capabilities
+                })
+            end
+
+            -- Lua с особыми настройками
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
+
+            -- Go с особыми настройками
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+                settings = {
+                    gopls = {
+                        analyses = {
+                            unusedparams = true,
+                        },
+                        staticcheck = true,
+                        gofumpt = true,
+                    },
+                },
+            })
+
+            -- Настройка остальных серверов
+            local servers = {
+                "ts_ls",
+                "html",
+                "cssls",
+                "intelephense",
+                "jsonls",
+                "yamlls",
+                "dockerls",
+                "marksman",
+                "sqlls"
+            }
+            for _, server in ipairs(servers) do
+                if server == "eslint" then
+                    lspconfig.eslint.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            -- Использовать локальный eslint
+                            useESLintClass = true,
+                            experimental = {
+                                useFlatConfig = true,
+                            },
+                            -- Проверять только нужные файлы
+                            validate = {
+                                "javascript",
+                                "javascriptreact",
+                                "typescript",
+                                "typescriptreact",
+                            },
+                        },
+                        on_attach = function(client, bufnr)
+                            -- Автоисправление при сохранении
+                            vim.api.nvim_create_autocmd("BufWritePre", {
+                                buffer = bufnr,
+                                command = "EslintFixAll",
+                            })
+                        end,
+                    })
+                else
+                    setup_lsp(server)
+                end
+            end
+
+            -- Маппинги
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+
+            -- LSP attachments
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set('n', '<leader>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+                end,
+            })
+        end,
+    },
+    -- Автодополнение
+    {
+        "hrsh7th/nvim-cmp",
+        pin = true,
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "rafamadriz/friendly-snippets", -- добавляем коллекцию готовых сниппетов
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            -- Загружаем сниппеты VSCode
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+                    -- Добавляем навигацию по Tab
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                }),
+                -- Улучшаем вид окна автодополнения
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+            })
+
+            -- Настройка автодополнения для командной строки
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' },
+                    { name = 'cmdline' }
+                })
+            })
+        end,
+    },
+    -- Форматирование через линтеры
+    {
+        "nvimtools/none-ls.nvim",
+        pin = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            local null_ls = require("null-ls")
+
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.prettier.with({
+                        prefer_local = "node_modules/.bin",
+                        condition = function(utils)
+                            return utils.root_has_file({
+                                ".prettierrc",
+                                ".prettierrc.json",
+                                ".prettierrc.yml",
+                                ".prettierrc.yaml",
+                                ".prettierrc.json5",
+                                ".prettierrc.js",
+                                "prettier.config.js",
+                                ".prettierrc.mjs",
+                                "prettier.config.mjs",
+                                ".prettierrc.cjs",
+                                "prettier.config.cjs"
+                            })
+                        end,
+                        -- Указываем только нужные типы файлов
+                        filetypes = {
+                            "javascript",
+                            "javascriptreact",
+                            "typescript",
+                            "typescriptreact",
+                        },
+                    })
+                },
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format({
+                                    async = false,
+                                    filter = function(c)
+                                        return c.name == "null-ls"
+                                    end
+                                })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    }
+}
+
+-- Загрузка плагинов
+require("lazy").setup(plugins)
---
+++ lua/options.lua
@@ -0,0 +1,90 @@
+--[[ Отступы и форматирование ]] --
+-- Настройки табуляции
+vim.o.expandtab = true -- Заменяет табы на пробелы
+vim.o.tabstop = 4 -- Ширина таба
+vim.o.softtabstop = 4 -- Количество пробелов при нажатии Tab
+vim.o.shiftwidth = 4 -- Количество пробелов для автоотступа
+
+-- Умные отступы
+vim.o.smartindent = true -- Умные отступы (например, после {)
+vim.o.autoindent = true -- Копирует отступ предыдущей строки
+vim.o.preserveindent = true -- Сохраняет структуру отступов
+vim.o.copyindent = true -- Копирует структуру отступов при автоотступе
+vim.o.breakindent = true -- Сохраняет отступы при переносе строк
+
+--[[ Интерфейс и отображение ]] --
+-- Нумерация строк
+vim.o.number = true -- Показывает номера строк
+vim.o.relativenumber = true -- Относительная нумерация строк
+vim.o.signcolumn = "yes" -- Столбец для знаков (git, диагностика)
+vim.opt.cursorline = true -- Подсветка текущей строки
+
+-- Цвета и отображение
+vim.o.showmode = false     -- Отключает показ режима в командной строке
+vim.opt.list = true        -- Показывает спецсимволы
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+--[[ Поиск и замена ]] --
+vim.o.ignorecase = true -- Игнорирует регистр при поиске
+vim.o.smartcase = true -- Учитывает регистр если есть символы в верхнем регистре
+vim.o.incsearch = true -- Интерактивный поиск
+vim.o.inccommand = "split" -- Интерактивный предпросмотр замен
+
+--[[ Файлы и буферы ]] --
+-- Файловые операции
+vim.o.swapfile = false -- Отключает создание swap файлов
+vim.o.backup = false -- Отключает создание резервных копий
+vim.o.undofile = true -- Сохраняет историю изменений
+vim.o.autowrite = true -- Автосохранение при переключении буферов
+vim.o.fileencoding = "utf-8" -- Кодировка файлов
+vim.o.confirm = true -- Подтверждение при закрытии несохраненных файлов
+
+-- Буферы и окна
+vim.o.splitbelow = true -- Новые окна открываются снизу
+vim.o.splitright = true -- Новые окна открываются справа
+
+--[[ Дополнительные функции ]] --
+-- Системные настройки
+vim.o.mouse = "a" -- Включает поддержку мыши
+vim.o.history = 1000 -- Размер истории команд
+vim.opt.clipboard:append("unnamed") -- Использование системного буфера обмена
+
+-- Автодополнение
+vim.o.completeopt = "menuone,noselect" -- Настройки автодополнения
+vim.o.pumheight = 15                   -- Максимальная высота меню автодополнения
+
+-- Форматирование
+vim.o.formatoptions = "jcroqlnt" -- Опции автоформатирования
+
+--[[ Маппинги клавиш ]] --
+-- Лидер
+vim.g.mapleader = " "
+-- Очистка поиска и терминал
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- Диагностика
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Ресайз окна
+vim.keymap.set({ "n", "v" }, "<M-K>", ":m .-2<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<M-J>", ":m .+1<CR>", { noremap = true, silent = true })
+---
+vim.keymap.set("n", "<M-l>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-h>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-k>", ":resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-j>", ":resize +2<CR>", { noremap = true, silent = true })
+
+---
+vim.keymap.set({ "n", "v" }, "<leader>w", ":bd<CR>", { noremap = true, silent = true })
+
+local function saveAndExecute()
+	vim.lsp.buf.format({})
+	vim.cmd("wa")
+end
+
+
+vim.keymap.set({ "n", "v", "i" }, "<C-s>", saveAndExecute, { noremap = true, silent = true })
+
+vim.api.nvim_create_user_command("Vb", function(opts)
+	vim.cmd("vert sb " .. opts.args)
+end, { nargs = 1 })
+
