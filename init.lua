271b43fefe73f13a192198dff3fd7ec30e1da1b3
Dmitriy Gerasimenko <dvgerasimenko@avito.ru>
2025-05-01 22:04:16 +0300

    Automatically add pair to some symbols

--- init.lua
+++ init.lua
@@ -169,6 +169,15 @@ local plugins = {
             options = { try_as_border = true },
         },
     },
+    {
+        "echasnovski/mini.pairs",
+        opts = {
+            modes = { insert = true, command = true, terminal = false },
+            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+            skip_ts = { "string" },
+            skip_unbalanced = true,
+        },
+    },
     -- Тема
     {
         "catppuccin/nvim",
